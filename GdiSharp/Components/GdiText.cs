4813fcc788397508036a46b1b6d7c319baf8ffe8
Pham Tung <pham.tung1@gmail.com>
2019-09-16 17:49:38 +0700

    Update GdiText

--- GdiSharp/Components/GdiText.cs
+++ GdiSharp/Components/GdiText.cs
@@ -10,13 +10,24 @@ namespace GdiSharp.Components
 
         public SlimFont Font { get; set; }
 
+        public bool IsTextAlignRight { get; set; }
+
         public override void Render(Graphics graphics)
         {
             using (var brush = new SolidBrush(this.Color))
             using (var font = this.Font.ToFatFont())
+            using (StringFormat stringFormat = new StringFormat())
             {
                 var position = GetAbsolutePosition(graphics);
-                graphics.DrawString(this.Content, font, brush, position);
+
+                if (this.IsTextAlignRight)
+                {
+                    var size = GetComponentSize(graphics);
+                    stringFormat.Alignment = StringAlignment.Far;
+                    position.X += size.Width;
+                }
+
+                graphics.DrawString(this.Content, font, brush, position, stringFormat);
             }
         }
 
